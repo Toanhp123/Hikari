b4dfb90d9ecf09ed1597670bb39166974ed1dd2c
Toanhp123 <toanpham12102003@gmail.com>
2026-08-12 01:07:07 +0700

    fix: repin bundled MangaDex package

--- app/src/main/kotlin/app/openstory/di/BundledPlugins.kt
+++ app/src/main/kotlin/app/openstory/di/BundledPlugins.kt
@@ -14,7 +14,7 @@ internal object BundledPlugins {
             assetPath = "plugins/mangadex-content.osp",
             pluginId = "org.openstory.content.mangadex",
             version = "1.1.0",
-            sha256 = "592cf603e910f4f333ad2dce77ef27af18bee540c6bfd1a66e5a9c47295cfb5d",
+            sha256 = "16127e1ae526390f5453d63a902e0210191ce90942aceebc0c693aa9e5587f54",
         ),
     )
 }
